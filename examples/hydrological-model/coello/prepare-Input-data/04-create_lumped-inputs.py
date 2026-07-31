239566a75f9dcbef08ec24110038521c66f319c9
Mostafa Farrag <moah.farag@gmail.com>
2026-08-01 00:10:59 +0200

    fix(examples): migrate the lumped-inputs script off the removed Hapi package and drop the dead visualizer example

--- examples/hydrological-model/coello/prepare-Input-data/04-create_lumped-inputs.py
+++ examples/hydrological-model/coello/prepare-Input-data/04-create_lumped-inputs.py
@@ -1,14 +1,18 @@
-"""
-Make sure the working directory is set to the examples folder in the Hapi repo"
-currunt_work_directory = Hapi/Example
+"""Create lumped (catchment-average) inputs from distributed rasters.
+
+Make sure the working directory is set to the root of the Hapi repo:
+current_work_directory = Hapi/
 """
 
+from __future__ import annotations
+
 import numpy as np
-from Hapi.rrm.inputs import Inputs as IN
+
+from hapi.inputs import Inputs
 
 rpath = "examples/hydrological-model/data/meteo_data/meteodata_prepared/"
 Path = f"{rpath}/temp-lumped-example"
 SaveTo = f"{rpath}/lumped_temp.txt"
 
-data = IN.createLumpedInputs(Path)
+data = Inputs.create_lumped_inputs(Path)
 np.savetxt(SaveTo, data, fmt="%7.2f")
